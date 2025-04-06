33ae154e4618e5ade6c9a78ffdf473a76a89f299
name <you@example.com>
2025-04-06 10:25:47 +0300

    feat: Youtube video player added to apod

--- src/entities/apod/ui/ApodItem/index.tsx
+++ src/entities/apod/ui/ApodItem/index.tsx
@@ -16,11 +16,27 @@ export function ApodItem({
 }: ApodItemProps) {
     return (
         <div className={styles.apodItem}>
-            <img src={url} />
-            <span className={styles.title}>{title}</span>
-            <span className={styles.date}>{date}</span>
-            <span className={styles.copyright}>{copyright}</span>
-            <span className={styles.explanation}>{explanation}</span>
+            <div className={styles.imaginary}>
+                {url.includes('youtube') ? (
+                    <iframe
+                        src={url}
+                        title="YouTube video player"
+                        autoFocus
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    ></iframe>
+                ) : (
+                    <img src={url} />
+                )}
+            </div>
+            <div className={styles.content}>
+                <header>
+                    <span className={styles.title}>{title}</span>
+                    <span className={styles.date}>{date}</span>
+                </header>
+                <span className={styles.copyright}>{copyright}</span>
+                <span className={styles.explanation}>{explanation}</span>
+            </div>
         </div>
     );
 }
